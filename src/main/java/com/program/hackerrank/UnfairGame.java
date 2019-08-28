49ffb3a449d1bc300314aaa14cd1c342f3f35d43
Ratnesh_Mishra <Ratnesh_Mishra@EPINHYDW0853.princeton.epam.com>
2019-08-28 15:17:38 +0530

    UnfairGame solution

---
+++ src/main/java/com/program/hackerrank/UnfairGame.java
@@ -0,0 +1,10 @@
+package com.program.hackerrank;
+
+public class UnfairGame {
+	// below url have the details description of this problem
+	// https://plus.maths.org/content/play-win-nim
+	public static int unfiarGame() {
+		return 0;
+
+	}
+}
